b6bc2a63b145190b34e41df698d749a83938e468
mpaulson <mpaulson@netflix.com>
2022-07-19 06:50:30 -0600

    feat: the ligma config

--- ligma.config.js
+++ ligma.config.js
@@ -4,52 +4,24 @@ module.exports = {
         "BinarySearchList",
         "TwoCrystalBalls",
         "BubbleSort",
-        "LinkedList",
+        "DoublyLinkedList",
+        "Queue",
+        "Stack",
         "ArrayList",
+        "MazeSolver",
+        "QuickSort",
+        "BTPreOrder",
+        "BTInOrder",
+        "BTPostOrder",
+        "BTBFS",
+        "CompareBinaryTrees",
+        "DFSOnBST",
+        // TODO:
+        // Red Black Tree
+        "DFSGraphList",
+        "BFSGraphList",
+        "BFSGraphMatrix",
+        "Map",
     ],
 }
-
-## Lists
-LinkedList
-* get length(): number;
-* remove(item: T): T | undefined;
-* removeAt(index: number): T | undefined;
-* append(item: T): void;
-* prepend(item: T): void;
-* get(index: number): T | undefined;
-
-Queue
-Stack
-
-ArrayList
-* Same api as list
-
-## Recursion
-The Maze Problem
-Quicksort
-
-## Trees
-Binary Tree Traversals
-DFS: pre
-DFS: in
-DFS: post
-
-BFS on Binary
-
-### Practice Problem
-- Compare Two Binary Trees
-
-DFS: Binary Search Tree Traversal
-
-RedBlack Trees
-
-## Graphs
-DFS
-BFS
-Dijkstra
-
-## Map
-Implement Map
-
-
 
