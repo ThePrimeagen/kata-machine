837395671784f5f7653f59c1b190518e488b5299
mpaulson <mpaulson@netflix.com>
2022-07-19 20:49:11 -0600

    feat: so many greater things now

--- scripts/align-configs.js
+++ scripts/align-configs.js
@@ -1,19 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports.package_json = function(config, day_path) {
-    const package_json = require("../package.json");
-    package_json.scripts.test = `jest ${config.dsa.join(" ")}`;
-    package_json.scripts.day = `echo ${day_path}`;
+module.exports.stats = function(config, day_path) {
+    let stats;
+    try {
+        stats = require("../stats.json");
+    } catch(e) {
+        stats = undefined;
+    }
 
-    package_json.kata_stats = config.dsa.reduce((acc, ds) => {
+    stats = config.dsa.reduce((acc, ds) => {
         if (!acc[ds]) {
             acc[ds] = 0;
         }
         acc[ds]++;
         return acc;
-    }, package_json.kata_stats || {});
+    }, stats || {});
 
+    fs.writeFileSync(
+        path.join(__dirname, "..", "stats.json"),
+        JSON.stringify(package_json, null, 4));
+}
+
+module.exports.package_json = function(config, day_path) {
+    const package_json = require("../package.json");
+    package_json.scripts.test = `jest ${config.dsa.join(" ")}`;
+    package_json.scripts.day = `echo ${day_path}`;
 
     fs.writeFileSync(
         path.join(__dirname, "..", "package.json"),
