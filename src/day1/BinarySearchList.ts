77072baa3d867587bada271482cb66149dd7f3f3
Gazmir Mazari <37550822+GazmirDi@users.noreply.github.com>
2022-11-28 22:55:39 -0500

    Add the buble sort and binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    
+    do{
+        const midpoint = Math.floor(low + (low + high) / 2);
+        const value = haystack[midpoint];
+        if (value === needle) {
+            return true;
+        }else if (value > needle) {
+            high = midpoint
+        }else {
+            low = midpoint + 1;
+        }
+    }while ( low < high)
+    return false
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jumpAmmount = Math.floor(Math.sqrt(breaks.length))
+
+    let i = jumpAmmount
+
+    for(; i < breaks.length; i += jumpAmmount) {
+        if(breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jumpAmmount
+
+    for (let j = 0; j < jumpAmmount && i < breaks.length; j++, i++) {
+        if(breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
