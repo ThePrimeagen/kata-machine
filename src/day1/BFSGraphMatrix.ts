1e5729deba7ad4fd461f6abc83e79856b82273fe
ddoyediran <ddoyediran@gmail.com>
2023-03-17 01:39:35 +0100

    initial commit

---
+++ src/day1/BFSGraphMatrix.ts
@@ -0,0 +1,3 @@
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+
+}
---
+++ src/day1/SinglyLinkedList.ts
@@ -0,0 +1,27 @@
+export default class SinglyLinkedList<T> {
+    public length: number;
+
+    
+
+    constructor() {
+    }
+
+    prepend(item: T): void {
+
+}
+    insertAt(item: T, idx: number): void {
+
+}
+    append(item: T): void {
+
+}
+    remove(item: T): T | undefined {
+
+}
+    get(idx: number): T | undefined {
+
+}
+    removeAt(idx: number): T | undefined {
+
+}
+}
