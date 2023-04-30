64a7f47e5e7a5a294c1463496806546763e64b25
loudbyte <loudbyte@loudbytes-iMac-Pro.local>
2023-04-30 21:07:23 +0200

    CrystalBalls: => final solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length))
 
+    let i = jmpAmount
+
+    for(; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpAmount
+
+    for ( let j = 0; j < breaks.length && i <= breaks.length; ++j, ++i) {
+        if (breaks[i]){
+            return i
+        }
+    }
+    return -1
 }
