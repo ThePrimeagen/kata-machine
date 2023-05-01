d95e860cac40220af7c7ffceeb093a03c39a6fa2
loudbyte <loudbyte@loudbytes-iMac-Pro.local>
2023-05-01 12:39:32 +0200

    BubbleSort: => bubbleSort final

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,11 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length - 1 - i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
 }
