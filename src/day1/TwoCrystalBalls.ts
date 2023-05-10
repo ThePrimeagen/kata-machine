1b5585ea4da7fad72530311b842c9702fba88801
personal <shaneel_kumar@live.com>
2023-05-10 23:49:56 +1200

    add two crystal balls algorithm

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+  let i = jumpAmount;
 
-}
+  for (; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) break;
+  }
+
+  i -= jumpAmount;
+
+  for (let j = 0; j < jumpAmount && i < breaks.length; i++, j++) {
+    if (breaks[i]) return i;
+  }
+
+  return -1;
+} 
