6584cfe210759701d238b48e5f01ad10716415b0
rinarudhei <rinaldiadrian5@gmail.com>
2023-06-12 21:40:20 +0700

    BTPostOrder BTPreOrder nicely done!

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,24 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+type BinaryNode<T> = {
+    value: T,
+    left: BinaryNode<T>|null,
+    right: BinaryNode<T>|null,
+}
 
+function walk(currNode: BinaryNode<number>|null, path: number[]) {
+    if (!currNode || !currNode.value) {
+        return;
+    }
+
+    walk(currNode.left, path);
+    walk(currNode.right, path);
+    path.push(currNode.value);
+
+    return;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    const path = [] as number[];
+    walk(head, path);
+
+    return path;
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,24 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+type BinaryNode<T> = {
+    value: T,
+    left: BinaryNode<T>|null,
+    right: BinaryNode<T>|null,
+}
 
+function walk(currNode: BinaryNode<number>|null, path: number[]) {
+    if (!currNode || !currNode.value) {
+        return;
+    }
+
+    path.push(currNode.value);
+    walk(currNode.left, path);
+    walk(currNode.right, path);
+
+    return;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path = [] as number[];
+    walk(head, path);
+
+    return path;
 }
