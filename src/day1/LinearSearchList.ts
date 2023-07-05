be429e119d8c1081fc7fdfcc2640224c547de1b7
Yisacc Aberham <isaccab2019@gmail.com>
2023-07-06 00:32:27 +0300

    add Binary search

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,11 @@
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            return true;
+        }
+    }
+    return false;
+}
