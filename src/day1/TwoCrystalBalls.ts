d8744396dc41d6f217c310abb36b82061b24e3d4
Yisacc Aberham <isaccab2019@gmail.com>
2023-07-07 14:38:48 +0300

    implement two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
+let i = jmpAmount;
+for (; i < breaks.length; i += jmpAmount) {
+    if (breaks[i]) {
+        break;
+    }
+}
+
+i -= jmpAmount;
+for (let j = 0; j <= jmpAmount && i < breaks.length; ++j, ++i) {
+    if (breaks[i]) {
+        return i;
+    }
+}
+
+return -1;
 }
