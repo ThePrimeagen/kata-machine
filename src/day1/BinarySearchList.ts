416a9d9389c4a9f73ac834711de0550e7da0d279
kanishkasd <kanishkasandaru2048@gmail.com>
2024-01-08 06:24:32 +0530

    binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[m]
 
+        if (v === needle) {
+            return true;
+        }
+        else if (v > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+
+    } while (lo < hi)
+        return false
 }
